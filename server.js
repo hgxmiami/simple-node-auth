364b6bc62c4a636bb7282e09af4e34e44ed2fac1
Hugo Giraldo <hgxmiami00@gmail.com>
2016-02-10 19:26:17 -0500

    update
    update

--- server.js
+++ server.js
@@ -3,22 +3,48 @@ var express = require('express');
 var app = express();
 var path = require('path');
 var bodyParser = require('body-parser');
+var crypto = require('crypto');
+
+var userStore = {
+  hugo: {hashedPassword: "7c6a180b36896a0a8c02787eeafb0e4c",
+         email: "hgxmiami00@gmail.com"},
+  kiki: {hashedPassword: "6cb75f652a9b52798eb6cf2201057c73",
+         email: "kikiqianlin@gmail.com"}
+};
 
 app.use( express.static('views') );
-app.use( bodyParser.urlencoded( { extended: false } ) );
+app.use(   bodyParser.urlencoded( {extended: false} )   );
 //more info on serving static files:
-// http://express.js.com/en/starter/static-files.html
+// http://expressjs.com/en/starter/static-files.html
 
-app.get("/", function(req,res){
-    res.sendFile(path.resolve("views/register.html"));
+app.get("/", function(req, res){
+  res.sendFile(path.resolve("views/register.html"));
 });
 
-//example of handline requestys parameters:
 
-app.get("/whatsmyname/:nameArg", function(req, res){
-  res.end("Your name is " + req.params.nameArg);  
+//REGISTRATION ROUTE
+app.post("/", function(req,res){
+  //if they didn't send a username or didnt send a
+  //password, respond with an error
+  if (!req.body.username || !req.body.password){
+    res.end("Username and password both required");
+    return;
+  }
+  var hash = crypto
+      .createHash("md5")
+      .update(req.body.password)
+      .digest('hex');
+  
+  var userNameInRequest = req.body.username;
+  userStore[userNameInRequest] = { hasedPassword: hash,
+                                  email: req.body.email};
+  res.end('Thanks for registering ' + userNameInRequest);
+  console.log(JSON.stringify(userStore));
 });
 
 
 
 app.listen(port);
+console.log("Server listening at port "+port);
+console.log("Current user store is :");
+console.log(JSON.stringify(userStore));
